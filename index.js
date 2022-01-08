1fc5d18f1c03dd0cdf4b068bb35bc6bdcc1aaca6
Bassem Dghaidi <link-@github.com>
2022-01-08 11:14:42 +0100

    Add main action structure

---
+++ index.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const main = async () => {
+  try {
+    /**
+     * We need to fetch all the inputs that were provided to our action
+     * and store them in variables for us to use.
+     **/
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const pr_number = core.getInput('pr_number', { required: true });
+    const token = core.getInput('token', { required: true });
+
+    /**
+     * Now we need to create an instance of Octokit which will use to call
+     * GitHub's REST API endpoints.
+     * We will pass token as an argument to the constructor. This token will
+     * be used to authenticate our requests.
+     * You can find all the information about how to use Octokit here:
+     * https://octokit.github.io/rest.js/v18
+     **/
+    const octokit = new github.getOctokit(token);
+
+    /**
+     * We need to fetch the list of files that were changes in the Pull Request
+     * and store them in a variable.
+     * We use octokit.paginate() to automatically loop over all the pages of the
+     * results.
+     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
+     */
+    const changedFiles = await octokit.paginate(
+      octokit.pulls.listFiles({
+        owner,
+        repo,
+        pull_number: pr_number,
+      })
+    );
+
+    console.log(changedFiles);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
