b67b1760d57e8528062a08c29b829c8d1ef33ff4
Bassem Dghaidi <link-@github.com>
2022-01-08 13:44:13 +0100

    Add pr summary logic

--- index.js
+++ index.js
@@ -35,12 +35,55 @@ const main = async () => {
       pull_number: pr_number,
     });
 
-    core.debug(`Changed files: ${JSON.stringify(changedFiles)}`);
 
-    // Loop over the files in changedFiles
+    /**
+     * Contains the sum of all the additions, deletions and changes
+     * in all the files in the Pull Request.
+     **/
+    let diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions;
+      acc.deletions += file.deletions;
+      acc.changes += file.changes;
+      return acc;
+    }, diffData);
+
+    /**
+     * Loop over all the files changed in the PR and add labels according 
+     * to files types.
+     **/
     for (const file of changedFiles) {
-      // Check if the file is a .md file
-      if (file.filename.endsWith('.md')) {
+      /**
+       * Add labels according to file types.
+       */
+      switch(file.filename.endsWith('.md')) {
+        case '.md':
+          await octokit.rest.issues.addLabels({
+            owner,
+            repo,
+            issue_number: pr_number,
+            labels: ['markdown'],
+          });
+        case '.js':
+          await octokit.rest.issues.addLabels({
+            owner,
+            repo,
+            issue_number: pr_number,
+            labels: ['javascript'],
+          });
+        case '.yml':
+          await octokit.rest.issues.addLabels({
+            owner,
+            repo,
+            issue_number: pr_number,
+            labels: ['yaml'],
+          });
+        case '.yaml':
+          await octokit.rest.issues.addLabels({
+            owner,
+            repo,
+            issue_number: pr_number,
+            labels: ['yaml'],
+          });
       }
     }
 
@@ -53,8 +96,10 @@ const main = async () => {
       repo,
       issue_number: pr_number,
       body: `
-        The files that are supposed to be changed: \n
-        ${JSON.stringify(changedFiles)}
+        Pull Request #${pr_number} has been updated with: \n
+        - ${diffData.changes} changes \n
+        - ${diffData.additions} additions \n
+        - ${diffData.deletions} deletions \n
       `
     });
 
@@ -63,4 +108,5 @@ const main = async () => {
   }
 }
 
+// Call the main function to run the action
 main();
